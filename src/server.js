50a607aa3dad7b60afba995a37dd46983f8b047b
fuyuan9 <fu9fu9fu9+fuyuan9@gmail.com>
2020-06-07 22:46:12 +0900

    Improve non chinese words

--- src/server.js
+++ src/server.js
@@ -1,22 +1,75 @@
 "use strict";
 const express = require("express");
 const serverless = require("serverless-http");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-const pinyin = require("pinyin");
+const pinyinConverter = require("pinyin");
+const PINYIN_DICT = require("pinyin/data/dict-zi");
 
 const router = express.Router();
 router.get("/", (req, res) => {
-  const query = decodeURI(req.query.q || "");
+  const input = decodeURI(req.query.q || "");
+  const letters = input.split("");
+  let chunks = [];
+  let nohans = "";
+  let pinyinList = [];
+  let outputBuff = [];
+  for (let i = 0; i < letters.length; i++) {
+    const letter = letters[i];
+    const firstCharCode = letter.charCodeAt(0);
+    if (PINYIN_DICT[firstCharCode]) {
+      if (nohans !== "") {
+        chunks.push({ isHanzi: false, str: nohans });
+        nohans = "";
+      }
+      chunks.push({ isHanzi: true, str: letter });
+    } else {
+      // non chinese
+      nohans = nohans + letter;
+    }
+  }
+  if (nohans != "") {
+    chunks.push({ isHanzi: false, str: nohans });
+    nohans = "";
+  }
+
+  for (let j = 0; j < chunks.length; j++) {
+    const token = chunks[j];
+    const isHanzi = token.isHanzi;
+    const str = token.str;
+    if (isHanzi) {
+      const pinyinData = pinyinConverter(str, {
+        heteronym: true,
+      });
+      pinyinList.push({
+        hanzi: str,
+        pinyin: pinyinData.length > 0 ? pinyinData[0] : [],
+      });
+      const pinyinDataString = pinyinData.map((item) => {
+        const _item = [...item];
+        const first = _item.shift();
+        const others = _item.length > 0 ? " (" + _item.join(", ") + ")" : "";
+        return first + others;
+      });
+      outputBuff.push(pinyinDataString);
+    } else {
+      pinyinList.push({
+        hanzi: str,
+        pinyin: [],
+      });
+      outputBuff.push(str);
+    }
+  }
+  const output = outputBuff.join(" ");
+
   res.json({
-    query: query,
-    pinyin: pinyin(query, {
-      heteronym: true,
-    }),
+    input: input,
+    output: output,
+    pinyin: pinyinList,
   });
 });
 
-app.use(cors())
+app.use(cors());
 app.use("/.netlify/functions/server", router);
 
 module.exports = app;
